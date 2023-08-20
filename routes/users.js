46ce0b478971bbbef0cf40eb7e96491227fb685a
Leo Kim <leobkkim@gmail.com>
2023-08-21 07:24:01 +0900

    feat: save new user info to database

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
 
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/:email", async function (req, res, next) {
+  const email = req.params.email;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    res.status(200).json({ existingUser, exists: true });
+  } else {
+    res.status(404).json({ exists: false });
+  }
+});
+
+router.post("/", async function (req, res, next) {
+  const { email, displayName, photoURL } = req.body.user;
+
+  const existingUser = await User.findOne({ email });
+
+  if (existingUser) {
+    return res.status(409).json({ message: "User already registered" });
+  }
+
+  await User.create({ email, username: displayName, photoURL });
+  res.status(201).json({ success: true, message: "User registered successfully" });
 });
 
 module.exports = router;
