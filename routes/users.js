1f6d79a6c3b384b9c5329e074b0353125afed6ad
Leo Kim <leobkkim@gmail.com>
2023-08-22 10:57:01 +0900

    feat: change login responses to sync with FE logic

--- routes/users.js
+++ routes/users.js
@@ -5,10 +5,10 @@ const User = require("../models/User");
 router.get("/:email", async function (req, res, next) {
   const email = req.params.email;
 
-  const existingUser = await User.findOne({ email });
+  const existingUser = await User.findOne({ email }).exec();
 
   if (existingUser) {
-    res.status(200).json({ existingUser, exists: true });
+    res.status(200).json({ existingUser });
   } else {
     res.status(404).json({ exists: false });
   }
@@ -17,14 +17,20 @@ router.get("/:email", async function (req, res, next) {
 router.post("/", async function (req, res, next) {
   const { email, displayName, photoURL } = req.body.user;
 
-  const existingUser = await User.findOne({ email });
+  try {
+    const existingUser = await User.findOne({ email }).exec();
 
-  if (existingUser) {
-    return res.status(409).json({ message: "User already registered" });
+    if (existingUser) {
+      return res.status(409).json({ message: "User already registered" });
+    } else {
+      const newUser = await User.create({ email, username: displayName, photoURL });
+
+      res.status(201).json({ newUser });
+    }
+  } catch (error) {
+    console.error("Error creating user", error);
+    res.status(500).json({ success: false, message: "User registration failed" });
   }
-
-  await User.create({ email, username: displayName, photoURL });
-  res.status(201).json({ success: true, message: "User registered successfully" });
 });
 
 module.exports = router;
