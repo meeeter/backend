658efa38a6cb0f0d2feb160ca30bde3fa4d10932
Leo Kim <leobkkim@gmail.com>
2023-08-23 14:57:11 +0900

    feat: update GET / endpoint; create GET /:userId endpoint

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,37 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 
-router.get("/:email", async function (req, res, next) {
-  const email = req.params.email;
+router.get("/", async (req, res, next) => {
+  const email = req.query.email;
 
-  const existingUser = await User.findOne({ email }).exec();
+  try {
+    const existingUser = await User.findOne({ email }).exec();
 
-  if (existingUser) {
-    res.status(200).json({ existingUser });
-  } else {
-    res.status(404).json({ exists: false });
+    if (existingUser) {
+      res.status(200).json({ existingUser });
+    } else {
+      res.status(404).json({ exists: false });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
-router.post("/", async function (req, res, next) {
+router.get("/:userId", async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    console.error("Error fetching user:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+router.post("/", async (req, res, next) => {
   const { email, displayName, photoURL } = req.body.user;
 
   try {
