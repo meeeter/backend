77feca5a5ce078e8d651e2a8058dfbf12032911d
Leo Kim <leobkkim@gmail.com>
2023-08-24 13:07:46 +0900

    feat: add GET friendRequest endpoint

--- routes/users.js
+++ routes/users.js
@@ -32,6 +32,30 @@ router.get("/:userId", async (req, res, next) => {
   }
 });
 
+router.get("/:userId/friend-requests", async (req, res, next) => {
+  try {
+    const userId = req.params.userId;
+    const user = await User.findById(userId)
+      .populate({
+        path: "friendRequestsSent.toUser friendRequestsReceived.fromUser",
+        model: "User",
+      })
+      .exec();
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const sentFriendRequests = user.friendRequestsSent;
+    const receivedFriendRequests = user.friendRequestsReceived;
+
+    res.status(200).json({ sentFriendRequests, receivedFriendRequests });
+  } catch (error) {
+    console.error("Error fetching friend requests:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
 router.post("/", async (req, res, next) => {
   const { email, displayName, photoURL } = req.body.user;
 
